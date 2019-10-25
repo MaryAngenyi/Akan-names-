f6257fa4b0c663930cbbbf61abe2694f4895d425
Mary Angenyi <maryangenyi001@gmail.com>
2019-10-25 15:50:59 +0300

    added female names

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var dayOfWeek=0,1,2,3,4,5,6;
+var maleNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+
+  
+
+var femaleNames= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
