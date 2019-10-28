302a0971f6385a75b3b06425c8eb3a51b5ea47b8
Mary Angenyi <maryangenyi001@gmail.com>
2019-10-28 03:54:40 +0300

    added formula for calculation

--- js/script.js
+++ js/script.js
@@ -26,6 +26,11 @@ var femaleAkanNames = [
   "Afua",
   "Ama"
 ];
+function getDayOfWeek() {
 var day = parseInt(document.getElementById("day").value);
 var month = parseInt(document.getElementById("month").value);
 var year = parseInt(document.getElementById("year").value);
+var yearString= year.toString();
+var century=parseInt(centuryString);
+var centuryString=yearString.slice(0,2);
+var dayOfWeek=math.trunc((((century/4)-2*century-1)+((5*year/4)+((26*(month+1/10))+date)%
