2124330f2320e3b1bdda03212bb7e30e528e5688
Mary Angenyi <maryangenyi001@gmail.com>
2019-10-28 07:14:26 +0300

    add labels to my inputs

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var dayOfWeek = [
   "Friday",
   "saturday"
 ];
-var maleAkanNames = [
+var malesNames = [
   "Kwasi",
   "Kwadwo",
   "Kwabena",
@@ -17,7 +17,7 @@ var maleAkanNames = [
   "Kwame"
 ];
 
-var femaleAkanNames = [
+var femalesNames = [
   "Akosua",
   "Adwoa",
   "Abenaa",
@@ -33,4 +33,57 @@ var year = parseInt(document.getElementById("year").value);
 var yearString= year.toString();
 var century=parseInt(centuryString);
 var centuryString=yearString.slice(0,2);
+
 var dayOfWeek=math.trunc((((century/4)-2*century-1)+((5*year/4)+((26*(month+1/10))+date)%
+}
+
+if(male1.checked==true){
+  switch(dayOfWeek)
+{}
+case 0:
+  alert("Your akan name is:" + malesNames[dayOfWeek]);
+  case 1:
+    alert("your akan name is:" + malesNames[dayOfWeek]);
+    case 2:
+
+    alert("your akan name is:" + malesNames[dayOfWeek]);
+    case 3:
+
+    alert("your akan name is:" + malesNames[dayOfWeek]);
+    case 4:
+
+    alert("your akan name is:" + malesNames[dayOfWeek]);
+    case 5:
+
+    alert("your akan name is:" + malesNames[dayOfWeek]);
+    cae 6:
+
+    alert("your akan name is:" + malesNames[dayOfWeek]);
+
+}
+}
+else if(female1.checked==true){
+  switch(dayOfWeek)
+{}
+case 0:
+  alert("Your akan name is:" + femalesNames[dayOfWeek]);
+  case 1:
+    alert("your akan name is:" + femalesNames[dayOfWeek]);
+    case 2:
+
+    alert("your akan name is:" + femalesNames[dayOfWeek]);
+    case 3:
+
+    alert("your akan name is:" + femalesNames[dayOfWeek]);
+    case 4:
+
+    alert("your akan name is:" + femalesNames[dayOfWeek]);
+    case 5:
+
+    alert("your akan name is:" + femalesNames[dayOfWeek]);
+    cae 6:
+
+    alert("your akan name is:" + femalesNames[dayOfWeek]);
+
+}
+
