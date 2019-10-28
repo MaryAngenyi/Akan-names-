0d133e7bbd5615f72897a2b55fd07b475588980c
Mary Angenyi <maryangenyi001@gmail.com>
2019-10-28 08:31:32 +0300

    linked javascript with index.html

--- js/script.js
+++ js/script.js
@@ -1,89 +1,31 @@
-var dayOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "wednesday",
-  "Thursday",
-  "Friday",
-  "saturday"
-];
-var malesNames = [
-  "Kwasi",
-  "Kwadwo",
-  "Kwabena",
-  "Kwaku",
-  "Yaw",
-  "Kofi",
-  "Kwame"
-];
+var submit = document.getElementById("submit");
 
-var femalesNames = [
-  "Akosua",
-  "Adwoa",
-  "Abenaa",
-  "Akua",
-  "Yaa",
-  "Afua",
-  "Ama"
-];
-function getDayOfWeek() {
-var day = parseInt(document.getElementById("day").value);
-var month = parseInt(document.getElementById("month").value);
-var year = parseInt(document.getElementById("year").value);
-var yearString= year.toString();
-var century=parseInt(centuryString);
-var centuryString=yearString.slice(0,2);
+ submit.addEventListener("click", function() {
+  var dayOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "wednesday",
+    "Thursday",
+    "Friday",
+    "saturday"
+  ];
+  var malesNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+  ];
 
-var dayOfWeek=math.trunc((((century/4)-2*century-1)+((5*year/4)+((26*(month+1/10))+date)%
-}
-
-if(male1.checked==true){
-  switch(dayOfWeek)
-{}
-case 0:
-  alert("Your akan name is:" + malesNames[dayOfWeek]);
-  case 1:
-    alert("your akan name is:" + malesNames[dayOfWeek]);
-    case 2:
-
-    alert("your akan name is:" + malesNames[dayOfWeek]);
-    case 3:
-
-    alert("your akan name is:" + malesNames[dayOfWeek]);
-    case 4:
-
-    alert("your akan name is:" + malesNames[dayOfWeek]);
-    case 5:
-
-    alert("your akan name is:" + malesNames[dayOfWeek]);
-    cae 6:
-
-    alert("your akan name is:" + malesNames[dayOfWeek]);
-
-}
-}
-else if(female1.checked==true){
-  switch(dayOfWeek)
-{}
-case 0:
-  alert("Your akan name is:" + femalesNames[dayOfWeek]);
-  case 1:
-    alert("your akan name is:" + femalesNames[dayOfWeek]);
-    case 2:
-
-    alert("your akan name is:" + femalesNames[dayOfWeek]);
-    case 3:
-
-    alert("your akan name is:" + femalesNames[dayOfWeek]);
-    case 4:
-
-    alert("your akan name is:" + femalesNames[dayOfWeek]);
-    case 5:
-
-    alert("your akan name is:" + femalesNames[dayOfWeek]);
-    cae 6:
-
-    alert("your akan name is:" + femalesNames[dayOfWeek]);
-
-}
-
+  var femalesNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+  ]
